5fff29a76ed96623d0e20f065410eecc04b0ad45
ZoyaYusuf <zoyayusuf04@gmail.com>
2026-02-25 03:15:45 +0530

    visit count added3

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Headline from './Components/Headline'
 import Projects from './Components/Projects'
 import Certifications from './Components/Certifications'
 import Contact from './Components/Contact' 
+import { useEffect } from "react"
 import { Analytics } from "@vercel/analytics/react"
 
 function App() { 
