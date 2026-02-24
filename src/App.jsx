b34ad23a24043670198513b3aa332800c45528cc
ZoyaYusuf <zoyayusuf04@gmail.com>
2026-02-25 03:18:21 +0530

    visit count removed

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,12 @@ import Headline from './Components/Headline'
 import Projects from './Components/Projects'
 import Certifications from './Components/Certifications'
 import Contact from './Components/Contact' 
-import { useEffect } from "react"
-import { Analytics } from "@vercel/analytics/react"
 
 function App() { 
 
   return (
     <>
       <div>
-        <Analytics/>
         <Navigation /> 
         <Headline/>
         <Skills /> 
